e31e664d8890353bfc981fcb339b7e5ca9622203
Kevin Roark <ker2143@columbia.edu>
2015-04-24 02:01:12 -0400

    initial round of god-man videos

---
+++ convert_mp4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+~/code/art/shane/mistershane/webm.sh video/game_1.mp4 video/game_1.webm
+~/code/art/shane/mistershane/webm.sh video/game_2.mp4 video/game_2.webm
+~/code/art/shane/mistershane/webm.sh video/lonely_cowboy.mp4 video/lonely_cowboy.webm
+~/code/art/shane/mistershane/webm.sh video/mojave_cycle.mp4 video/mojave_cycle.webm
+~/code/art/shane/mistershane/webm.sh video/softypapa.mp4 video/softypapa.webm
+~/code/art/shane/mistershane/webm.sh video/vegas.mp4 video/vegas.webm
+
+~/code/art/shane/mistershane/webm.sh video/vin_diesel.mp4 video/vin_diesel.webm
